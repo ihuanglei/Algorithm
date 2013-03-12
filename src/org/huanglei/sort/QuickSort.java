9b283db0bab95c859278c1dc5f810eba57dbdff6
code-huanglei <code.huanglei@gmail.com>
2013-03-12 17:23:55 +0800

    QuickSort

--- src/org/huanglei/sort/QuickSort.java
+++ src/org/huanglei/sort/QuickSort.java
@@ -15,21 +15,27 @@ public class QuickSort implements Sort {
 	}
 
 	private void _sort(int[] a, int from, int to) {
+
+		int fromTmp = from, toTmp = to;
+
 		if (from < to) {
-			int temp = a[to];
-			int i = from - 1;
-			for (int j = from; j < to; j++) {
-				if (a[j] <= temp) {
-					i++;
-					int tempValue = a[j];
-					a[j] = a[i];
-					a[i] = tempValue;
+			int pivokey = a[from];
+			while (from < to) {
+				while (from < to && a[to] > pivokey) {
+					to--;
 				}
+				if (from < to)
+					a[from++] = a[to];
+				while (from < to && a[from] < pivokey) {
+					from++;
+				}
+				if (from < to)
+					a[to--] = a[from];
 			}
-			a[to] = a[i + 1];
-			a[i + 1] = temp;
-			_sort(a, from, i);
-			_sort(a, i + 1, to);
+			a[from] = pivokey;
+			_sort(a, fromTmp, from - 1);
+			_sort(a, from + 1, toTmp);
+
 		}
 
 	}
